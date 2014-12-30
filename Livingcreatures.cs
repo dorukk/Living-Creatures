f752d9c956b3ff2e27fcd945c87ee2fa020a1ccd
Umur Doruk Kaşoğlu <dorukkasoglu@gmail.com>
2014-12-30 10:54:09 +0200

    Create Livingcreatures.cs

---
+++ Livingcreatures.cs
@@ -0,0 +1,14 @@
+class livingcreatures
+    {
+        public string data = "The info about alives ";
+        public int year;
+        
+        public  void eat()
+        {
+
+        }
+        public void sleep()
+        {
+
+        }
+    }
